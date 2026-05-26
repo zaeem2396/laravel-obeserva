efeb9daaaf16cb0c9d7fab2d1488a297029a7ede
zaeem2396 <zaeemansari87@gmail.com>
2026-05-26 14:35:09 +0530

    feat(core): add active span stack to ContextManager.

--- packages/core/src/Context/ContextManager.php
+++ packages/core/src/Context/ContextManager.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Obeserva\Core\Context;
 
+use Obeserva\Contracts\Driver\ActiveSpanStorageInterface;
 use Obeserva\Contracts\Driver\ContextStorageInterface;
+use Obeserva\Contracts\Span\SpanInterface;
 use Obeserva\Contracts\Trace\TraceContextInterface;
 
-final class ContextManager implements ContextStorageInterface
+final class ContextManager implements ActiveSpanStorageInterface, ContextStorageInterface
 {
     private ?TraceContextInterface $context = null;
+
+    /** @var list<SpanInterface> */
+    private array $activeSpans = [];
 
     public function get(): ?TraceContextInterface
     {
@@ -24,5 +29,27 @@ final class ContextManager implements ContextStorageInterface
     public function clear(): void
     {
         $this->context = null;
+        $this->activeSpans = [];
+    }
+
+    public function current(): ?SpanInterface
+    {
+        if ($this->activeSpans === []) {
+            return null;
+        }
+
+        return $this->activeSpans[array_key_last($this->activeSpans)];
+    }
+
+    public function push(SpanInterface $span): void
+    {
+        $this->activeSpans[] = $span;
+    }
+
+    public function pop(): void
+    {
+        if ($this->activeSpans !== []) {
+            array_pop($this->activeSpans);
+        }
     }
 }
